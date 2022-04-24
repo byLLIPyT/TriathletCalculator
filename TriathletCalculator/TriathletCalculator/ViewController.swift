29974ce86c52a43cbe233d9164b49153eed37621
Utkin Aleksandr <byllipyt@icloud.com>
2022-04-24 19:24:12 +0300

    +initial commit

---
+++ TriathletCalculator/TriathletCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriathletCalculator
+//
+//  Created by Александр Уткин on 24.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
