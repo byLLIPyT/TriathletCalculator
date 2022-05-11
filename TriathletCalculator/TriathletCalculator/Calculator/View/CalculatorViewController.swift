3c57fa4dcaec4f1eda5961dc7d9f76fb31fbfd00
Utkin Aleksandr <byllipyt@icloud.com>
2022-05-11 13:26:10 +0300

    +fix

--- TriathletCalculator/TriathletCalculator/Calculator/View/CalculatorViewController.swift
+++ TriathletCalculator/TriathletCalculator/Calculator/View/CalculatorViewController.swift
@@ -9,21 +9,57 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    
+    //MARK: - Swim
+    
+    @IBOutlet weak var swimHoursTime: UITextField!
+    @IBOutlet weak var swimMinutesTime: UITextField!
+    @IBOutlet weak var swimSecondsTime: UITextField!
+    @IBOutlet weak var swimMinutesPace: UITextField!
+    @IBOutlet weak var swimSecondsPace: UITextField!
+    @IBOutlet weak var swimSpeed: UITextField!
+    
+    //MARK: - Bike
+    
+    @IBOutlet weak var bikeHoursTime: UITextField!
+    @IBOutlet weak var bikeMinutesTime: UITextField!
+    @IBOutlet weak var bikeSecondsTime: UITextField!
+    @IBOutlet weak var bikeMinutesPace: UITextField!
+    @IBOutlet weak var bikeSecondsPace: UITextField!
+    @IBOutlet weak var bikeSpeed: UITextField!
+    
+    //MARK: - Run
+    
+    @IBOutlet weak var runHoursTime: UITextField!
+    @IBOutlet weak var runMinutesTime: UITextField!
+    @IBOutlet weak var runSecondsTime: UITextField!
+    @IBOutlet weak var runMinutesPace: UITextField!
+    @IBOutlet weak var runSecondsPace: UITextField!
+    @IBOutlet weak var runSpeed: UITextField!
+    
+    //MARK: - Transit zones
+    
+    @IBOutlet weak var firstTransitMinutes: UITextField!
+    @IBOutlet weak var firstTransitSeconds: UITextField!
+    @IBOutlet weak var secondTransitMinutes: UITextField!
+    @IBOutlet weak var secondTransitSeconds: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButton(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction func changeTypeRace(_ sender: Any) {
+        
+    }
+    
+    @IBAction func changeTypeMeasures(_ sender: Any) {
+        
+    }
+    
 }
