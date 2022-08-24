3ebf873749d61192015d10a2ee5a5e5e13a3c399
Utkin Aleksandr <byllipyt@icloud.com>
2022-08-24 20:37:46 +0300

    +

--- TriathletCalculator/TriathletCalculator/Calculator/View/CalculatorViewController.swift
+++ TriathletCalculator/TriathletCalculator/Calculator/View/CalculatorViewController.swift
@@ -217,7 +217,11 @@ class CalculatorViewController: UIViewController,UITextFieldDelegate {
     //MARK: - Private functions
     
     private func updateUI() {
+<<<<<<< HEAD
         totalTimeLabel.text = "20:30:00"
+=======
+        totalTimeLabel.text = "01.01.01"
+>>>>>>> cd236f784f299c1c34c165feec82417ab3ce4283
     }
     
     private func calculateWhenTimeChanged(sportType: SportType) {
